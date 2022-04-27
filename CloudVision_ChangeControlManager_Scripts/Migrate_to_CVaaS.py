85d01f23860b478ec458f424180384aeb838b2fc
philippebureau <philippe.bureau2@gmail.com>
2022-04-27 09:38:33 -0400

    Update Migrate_to_CVaaS.py

--- CloudVision_ChangeControlManager_Scripts/Migrate_to_CVaaS.py
+++ CloudVision_ChangeControlManager_Scripts/Migrate_to_CVaaS.py
@@ -17,7 +17,7 @@ alog("Migrating node to CVaaS")
 # Migration
 try:
     target.runCmds(["enable", \
-     "bash timeout 2 echo '%s' > /tmp/cv-onboarding-token" % (TOKEN), \
+      {"cmd": "copy terminal: file:/tmp/cv-onboarding-token", "input": TOKEN}, \
      "configure", \
      "daemon TerminAttr", \
      "exec /usr/bin/TerminAttr -cvaddr=apiserver.arista.io:443 -cvcompression=gzip -cvvrf=%s -taillogs -cvauth=token-secure,/tmp/cv-onboarding-token -smashexcludes=ale,flexCounter,hardware,kni,pulse,strata -ingestexclude=/Sysdb/cell/1/agent,/Sysdb/cell/2/agent" % (VRF), \
